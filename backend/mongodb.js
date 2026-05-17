c722e34a19edcfaad72e553f2cbaf42d5a04957d
Sourajit Das <sourajit8906@gmail.com>
2026-05-17 14:49:40 +0530

    moved mongo url to .env

--- backend/mongodb.js
+++ backend/mongodb.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(`mongodb://Sourajit:Nt5nE3Vm6RuuDbGp@ac-xmcc4va-shard-00-00.cdy3vz9.mongodb.net:27017,ac-xmcc4va-shard-00-01.cdy3vz9.mongodb.net:27017,ac-xmcc4va-shard-00-02.cdy3vz9.mongodb.net:27017/TodoList?ssl=true&replicaSet=atlas-432qsv-shard-0&authSource=admin&appName=Cluster0`);
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URL);
 
 const Schema = mongoose.Schema({
     title:String,
